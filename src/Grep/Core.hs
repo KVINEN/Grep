5da5b2a87224b70bda48fb6aaa009f9191271f3e
KVINEN <benjamin.roiland@hotmail.no>
2025-02-13 11:50:25 +0100

    started implementing fetchVals to validate input

--- src/Grep/Core.hs
+++ src/Grep/Core.hs
@@ -1,16 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Grep.Core where
 
 import qualified System.Environment as Sys
+import qualified Data.Text as T
+import           Data.Text (Text)
+import qualified Data.Text.IO as T
+import           System.Directory
 
-fetchVals :: IO (Either text [String])
+data LocationType = DirectoryType | FileType deriving (Eq,Show)
+
+data SearchType = SearchType {
+      searchTerm :: Text
+    , searchLocation :: filepath
+    , searchLocationType :: LocationType
+} deriving (Eq,Show)
+
+fetchVals :: IO (Either Text SearchType)
 fetchVals = do
     argList <- Sys.getArgs
-    if (length argList < 2) then putStrLn "Please provide correct arguments"
-
+    if (length argList < 2) then pure $ Left "Please provide searchTerm and searchLocation as arguments" else do
+        let searchLocation = argList !! 1
+        fb <- doesFileExist searchLocation
+        db <- doesDirectoryExist searchLocation
+        if (fb) then pure $ Right $ SearchType (head argList) searchLocation FileType
+        if (fb || db) then pure $ Left ("Search Location not valid, must be either filepath or directory path " <> (T.pack searchLocation)) else pure $ Right argList
 
 main' :: IO ()
 main' = do
-    eRes = fetchVals
+    eRes <- fetchVals
     case eRes of
         Left e -> T.putStrLn e
-        Right 
+        Right r -> processSearching r
+
+processSearching :: [String] -> IO ()
+processSearching = undefined
